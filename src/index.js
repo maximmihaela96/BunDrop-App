ac802df6cd5bbe2ef596b585bd490bbfa5726907
Mihaela Maxim <maxim.mihaela1996@gmail.com>
2023-08-24 15:18:55 +0200

    Update Index

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Router>
